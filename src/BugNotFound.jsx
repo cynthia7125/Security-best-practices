50abce11a83a4d3f270f9198cf1db18ea233f7c1
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-09-16 10:45:40 +0300

    Remove refs to prevent direct DOM manipulation

--- src/BugNotFound.jsx
+++ src/BugNotFound.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
-export function BugNotFound(props) {
-    return (
-        <h2>Bug "{props.id}" not found</h2>
-    );
+export class BugNotFound extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    container.innerHTML = `Bug "${this.props.id}" not found`;
+  }
+
+  render() {
+    return <h2>Bug "{this.props.id}" not found</h2>;
+  }
 }
